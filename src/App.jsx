160f40de32f4d959ddb146b0f04df76b46e10c7a
Kiran Gautam <ukiras@gmail.com>
2023-12-06 11:54:34 +1100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import prankSound from './assets/prank.wav';
 import CustomButton from './components/CustomButton'
 import { BUTTONS } from './utils'
+const audio = new Audio(prankSound);
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
   // }
 
   const [strToDisplay, setStrToDisplay] = useState("")
+  const [lastOperator, setLastOperator] = useState("")
   const [isPrank, setIsPrank] = useState(false);
 
   const operators = ["+", "-", "*", "/", "%"];
@@ -33,46 +35,90 @@ function App() {
     //   Prank Case
     if (random < 4) {
       setIsPrank(true)
-      const audio = new Audio(prankSound);
       audio.play();
       totalNumber += random;
     }
     setStrToDisplay(totalNumber.toString());
   };
-  
 
-  const handleOnClick = (value)=> {
+
+  const handleOnClick = (value) => {
+    setIsPrank(false);
     if (value === "AC") {
       setStrToDisplay("")
       return;
     }
 
-      // C should remove the last char
-      if (value === "C") {
-        return(setStrToDisplay(strToDisplay.slice(0, -1)));
-      }
+    // C should remove the last char
+    if (value === "C") {
+      return (setStrToDisplay(strToDisplay.slice(0, -1)));
+    }
 
 
-      // Get total
+    // Get total
     if (value === "=") {
       return getTotal();
     }
 
-      setStrToDisplay(strToDisplay + value)
+    if (value === ".") {
+      // If anyone pressed . in the beginning
+      if (strToDisplay === "") {
+        return setStrToDisplay(strToDisplay + "0.");
+      }
 
+      //   If anyone pressed . twice in a row
+      const lastChar = strToDisplay.slice(-1);
+      if (lastChar === ".") {
+        return;
+      }
+      //    Do not allow . if there is already . in string
+      //   if (strToDisplay.includes(".")) {
+      //     return;
+      //   }
+      //   Check if . exists in string to dispay after lastOperator
+      if (!lastOperator) {
+        if (strToDisplay.includes(".")) {
+          return;
+        }
+      }
+      const lastOperatorIndex = strToDisplay.lastIndexOf(lastOperator);
+      const lastNumberSet = strToDisplay.slice(lastOperatorIndex);
+      if (lastNumberSet.includes(".")) {
+        return;
+      }
+    }
+
+    if (strToDisplay.length === 0 && operators.includes(value)) {
+      return;
+    }
+
+    // Operator conditions
+    if (operators.includes(value)) {
+      setLastOperator(value)
+      const lastChar = strToDisplay.slice(-1);
+      if (operators.includes(lastChar)) {
+        return;
+      }
+      if (lastChar === ".") {
+        setStrToDisplay(strToDisplay + "0")
+      }
+    }
+
+
+    setStrToDisplay(strToDisplay + value)
   }
   return (
     <>
-     <div className="wrapper">
-    <h1>Prank Calculator</h1>
-    <small>Disclaimer: Ans might be incorrect in case you are pranked</small>
-    <div className="calculator">
-      <div className={isPrank ? "display prank" : 'display'}>{strToDisplay}</div>
-      {BUTTONS.map(button => {
-        return <CustomButton key={button.label} cls={button.cls} label={button.label} clickHandler={handleOnClick}/>
-      })}
-    </div>
-  </div>
+      <div className="wrapper">
+        <h1>Prank Calculator</h1>
+        <small>Disclaimer: Ans might be incorrect in case you are pranked</small>
+        <div className="calculator">
+          <div className={isPrank ? "display prank" : 'display'}>{strToDisplay}</div>
+          {BUTTONS.map(button => {
+            return <CustomButton key={button.label} cls={button.cls} label={button.label} clickHandler={handleOnClick} />
+          })}
+        </div>
+      </div>
     </>
   )
 }
--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-function CustomButton({ label, cls, clickHandler }) {
-    return (
+function CustomButton({label, cls, clickHandler}) {
+    return (            
         <div className={cls} onClick={() =>clickHandler(label)} >{label}</div>
     )
 }
-
+CustomButton.propTypes = {
+    label: PropTypes.string,
+    cls: PropTypes.string,
+    clickHandler: PropTypes.func,
+}
 export default CustomButton
